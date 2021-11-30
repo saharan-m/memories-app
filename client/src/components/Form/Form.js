ca1bbbc74ab1a5af144f04c7f7c62576afce90a5
saharan-m <mayanksaharan18@gmail.com>
2021-11-30 15:59:56 +0530

    after implementing auth

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -8,8 +8,8 @@ import { createPost,updatePost } from "./../../actions/posts";
 const Form = ({currentId,setCurrentId}) => {
   const post = useSelector(state=>currentId?state.posts.find(post=>post._id===currentId):null)
     const dispatch = useDispatch()
+    const user = JSON.parse(localStorage.getItem('profile'));
     const [postData,setPostData] = useState({
-        creator:'',
         title:'',
         message:'',
         tags:'',
@@ -27,15 +27,23 @@ const Form = ({currentId,setCurrentId}) => {
     //   console.log(postData.selectedFile)
     // console.log(postData)
     if(currentId){
-      dispatch(updatePost(currentId,postData))
+      dispatch(updatePost(currentId,{...postData,name:user?.result?.name}))
     }else{
-      dispatch(createPost(postData))}
+      dispatch(createPost({...postData,name:user?.result?.name}))}
       clear()
   };
+  if(!user?.result?.name){
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant='h6' align='center'>
+          Please sign in to create and like other memories!
+        </Typography>
+      </Paper>
+    )
+  }
   const clear=()=>{
       setCurrentId(null);
       setPostData({
-        creator:'',
         title:'',
         message:'',
         tags:'',
@@ -51,7 +59,6 @@ const Form = ({currentId,setCurrentId}) => {
         onSubmit={submitHandler}
       >
         <Typography variant="h6">{!currentId?'Creating':'Editing'} a Memory</Typography>
-        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=>setPostData({...postData,creator:e.target.value})}/>
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e)=>setPostData({...postData,title:e.target.value})}/>
         <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e)=>setPostData({...postData,message:e.target.value})}/>
         <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e)=>setPostData({...postData,tags:e.target.value.split(',')})}/>
